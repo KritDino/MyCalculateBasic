9b85ee8ed7051aac11849dcee42201069eb5506b
krit <krit_san@hotmail.co.th>
2018-03-31 15:16:40 +0700

    Initial commit

---
+++ app/src/main/java/sanguannapaporn/krit/mycalculatebasic/MainActivity.java
@@ -0,0 +1,22 @@
+package sanguannapaporn.krit.mycalculatebasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//         Add Fragment to activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+        }
+
+
+
+
+    } // Main Method
+}   // Main Class
